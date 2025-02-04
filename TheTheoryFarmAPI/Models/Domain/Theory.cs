16e37cfdaaa7e6f3dcdfbd7c1dc1f41b8ff6ba49
aLearningLad <mocmanca@gmail.com>
2025-02-04 21:27:00 +0200

    slight changes to datetime property, automated to set date when theory object is instantiated

--- TheTheoryFarmAPI/Models/Domain/Theory.cs
+++ TheTheoryFarmAPI/Models/Domain/Theory.cs
@@ -1,13 +1,20 @@
-﻿namespace TheTheoryFarmAPI.Models.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheTheoryFarmAPI.Models.Domain
 {
     public class Theory
     {
         public Guid Id { get; set; }
 
+        [Required]
+        [MaxLength(40, ErrorMessage = "The joke's title cannot be longer than 40 characters")]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(50, ErrorMessage = "The joke's length cannot be shorter than 50 characters")]
+        [MaxLength(250, ErrorMessage = "The joke's length cannot be longer than 250 characters")]
         public string TheoryBody { get; set; }
 
-        public DateTime TheoryDate { get; set; } // google this
+        public DateTime PublishedDate { get; set; } = DateTime.UtcNow; // sets date automatically when theory object is instantiated. Great
     }
 }
