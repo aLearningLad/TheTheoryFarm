7def47b74e3e75b083adac7b5dfc4ac4ad551f0d
aLearningLad <mocmanca@gmail.com>
2025-02-04 10:36:54 +0200

    Initial theory model and properties

---
+++ TheTheoryFarmAPI/Models/Domain/Theory.cs
@@ -0,0 +1,13 @@
+﻿namespace TheTheoryFarmAPI.Models.Domain
+{
+    public class Theory
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string TheoryBody { get; set; }
+
+        public DateTime TheoryDate { get; set; } // google this
+    }
+}
