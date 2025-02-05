d91abac190dd618f1cca57d96ac7a1504829023c
aLearningLad <mocmanca@gmail.com>
2025-02-05 10:41:42 +0200

    Slight changes and migrations run

---
+++ TheTheoryFarmAPI/Migrations/20250205083545_theory tables setup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheTheoryFarmAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class theorytablessetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Theories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    TheoryBody = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    PublishedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Theories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Theories");
+        }
+    }
+}
