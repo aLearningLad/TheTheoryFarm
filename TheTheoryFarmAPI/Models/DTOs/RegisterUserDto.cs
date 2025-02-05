c7f080783b5497b2dbc5d2cfa0cfbbb17485a47a
aLearningLad <mocmanca@gmail.com>
2025-02-06 00:43:33 +0200

    user registration dto data annotations added

--- TheTheoryFarmAPI/Models/DTOs/RegisterUserDto.cs
+++ TheTheoryFarmAPI/Models/DTOs/RegisterUserDto.cs
@@ -1,6 +1,17 @@
-﻿namespace TheTheoryFarmAPI.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheTheoryFarmAPI.Models.DTOs
 {
     public class RegisterUserDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Email has to be at least 5 characters long")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Password has to be at least 8 characters long")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
